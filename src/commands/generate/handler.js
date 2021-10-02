ef4115ffab790cdb262097fa3e2d39a8a3c87318
SaViGnAnO <brian@ataxica.com>
2021-10-02 08:49:12 -0700

    add dotnet tool usage

--- src/commands/generate/handler.js
+++ src/commands/generate/handler.js
@@ -5,6 +5,9 @@ const xml2js = require('xml2js');
 const fs = require('fs');
 const child_process = require('child_process');
 const util = require('util');
+const {
+    group
+} = require('console');
 const exec = util.promisify(child_process.exec);
 
 const BASE_URL = 'https://dl.google.com/android/maven2/';
@@ -32,6 +35,8 @@ class Handler {
                 if (!downloadAll.toBeDumb) {
                     return this.handle(options);
                 }
+
+                groups.groupIds = await this.GetMasterIndex;
             }
 
             let configObject = [{
@@ -176,7 +181,10 @@ class Handler {
         if (options.bindings) {
             //await this.InstallDotnetTool();
             //RunProcess("xamarin-android-binderator", $"--config=\"{configFile}\" --basepath=\"{basePath}\"");
-            const {stdout, stderr} = await exec(`xamarin-android-binderator --config="${options.bindings}" --basepath="./output"`);
+            const {
+                stdout,
+                stderr
+            } = await exec(`xamarin-android-binderator --config="${options.bindings}" --basepath="${__dirname}\\output"`);
             if (stdout) {
                 console.log(stdout);
             }
@@ -188,7 +196,10 @@ class Handler {
     }
 
     async InstallDotnetTool() {
-        const {stdout, stderr} = await exec(`dotnet tool install -g xamarin.androidbinderator.tool`);
+        const {
+            stdout,
+            stderr
+        } = await exec(`dotnet tool install -g xamarin.androidbinderator.tool`);
         if (stdout) {
             console.log(stdout);
         }
@@ -200,7 +211,10 @@ class Handler {
 
     async askForMavenPackages() {
         const choices = (await this.GetMasterIndex).reduce((result, item, index) => {
-            result.push({name: item, value: item});
+            result.push({
+                name: item,
+                value: item
+            });
             return result;
         }, []);
 
@@ -208,12 +222,12 @@ class Handler {
             input = input || '';
 
             var fuzzyResult = fuzzy.filter(input, choices, {
-                extract: function(item) {
+                extract: function (item) {
                     return item['name'];
                 }
             });
 
-            var data = fuzzyResult.map(function(element) {
+            var data = fuzzyResult.map(function (element) {
                 return element.original;
             });
 
@@ -241,21 +255,21 @@ class Handler {
                     'Content-Type': 'application/xml',
                 },
             };
-        
+
             const parser = new xml2js.Parser();
             const response = await axios(config);
             const groups = Object.keys((await parser.parseStringPromise(response.data)).metadata);
-        
+
             return groups;
         })();
     }
 
-    async GetGroupIndex (groupIds) {
+    async GetGroupIndex(groupIds) {
         let groupIndex = [];
 
         function convertVersionsToArray(value, name) {
             if (typeof value === 'string' && name === 'versions') {
-                if (value.includes(',')){
+                if (value.includes(',')) {
                     value = value.split(',').reverse();
                 } else {
                     value = [value];
@@ -263,7 +277,7 @@ class Handler {
             }
             return value;
         }
-        
+
         for (const groupId in groupIds) {
             const url = `${BASE_URL}${groupIds[groupId].replace(/\./g, '/')}/group-index.xml`;
             const config = {
@@ -273,49 +287,63 @@ class Handler {
                     'Content-Type': 'application/xml',
                 },
             };
-        
-            const parser = new xml2js.Parser({ explicitArray: false, mergeAttrs: true, attrValueProcessors: [convertVersionsToArray] });
+
+            const parser = new xml2js.Parser({
+                explicitArray: false,
+                mergeAttrs: true,
+                attrValueProcessors: [convertVersionsToArray]
+            });
             const response = await axios(config);
 
 
             var groupdata = await parser.parseStringPromise(response.data);
             await Promise.resolve(
                 Object.keys(groupdata).reduce(async (result, key, _index) => {
-                await Promise.all(Object.keys(groupdata[key]).map(async (key2, _index2) => {
-                    const versionNum = groupdata[key][key2].versions[0];
-                    const artifactMedataUrl = `${BASE_URL}${groupIds[groupId].replace(/\./g, '/')}/${key2}/${versionNum}/artifact-metadata.json`
-                    const artifactRequestConfig = {
-                        method: 'get',
-                        url: artifactMedataUrl,
-                        responseType: 'json',
-                    };
-                    const artifactMetadataResponse = await axios(artifactRequestConfig);
-                    const artifactMetadata = artifactMetadataResponse.data.artifacts;
-                    result.push({
-                        groupId: groupIds[groupId],
-                        artifactId: key2,
-                        version: versionNum,
-                        nugetVersion: versionNum,
-                        nugetId: `savi.${groupIds[groupId]}.${key}`,
-                        dependencyOnly: false,
-                        downloadJavaSourceJars: artifactMetadata.filter(am => am.name.includes('-sources.jar')).length > 0,
-                        downloadPoms: artifactMetadata.filter(am => am.name.includes('.pom')).length > 0,
-                        downloadJavaDocJars: artifactMetadata.filter(am => am.name.includes('javadoc.jar')).length > 0,
-                        downloadMetadataFiles: artifactMetadata.filter(am => am.name.includes('-metadata.jar')).length > 0
-                    });
-                }));
-                return result;
-                
-            }, groupIndex));
-            
+                    await Promise.all(Object.keys(groupdata[key]).map(async (key2, _index2) => {
+                        const versionNum = groupdata[key][key2].versions[0];
+                        const artifactMedataUrl = `${BASE_URL}${groupIds[groupId].replace(/\./g, '/')}/${key2}/${versionNum}/artifact-metadata.json`
+                        const artifactRequestConfig = {
+                            method: 'get',
+                            url: artifactMedataUrl,
+                            responseType: 'json',
+                            validateStatus() {
+                                return true;
+                            }
+                        };
+                        const artifactMetadataResponse = await axios(artifactRequestConfig);
+                        const artifactMetadata = artifactMetadataResponse.data.artifacts || [];
+                        // Prevent 404 errors in savi-binderator tool
+                        const downloadJavaSourceJars =  artifactMetadata.filter(am => am.name.includes('-sources.jar')).length > 0;
+                        const downloadPoms = artifactMetadata.filter(am => am.name.includes('.pom')).length > 0;
+                        const downloadJavaDocJars =  artifactMetadata.filter(am => am.name.includes('javadoc.jar')).length > 0;
+                        const downloadMetadataFiles = artifactMetadata.filter(am => am.name.includes('-metadata.jar')).length > 0;
+                        const hasAar = artifactMetadata.filter(am => am.name.includes('.aar')).length > 0;
+                        const isNotDependency = downloadJavaSourceJars || hasAar;
 
-        function VersionSplit(value, _name) {
-            return value.split(',').reverse();
+                        result.push({
+                            groupId: groupIds[groupId],
+                            artifactId: key2,
+                            version: versionNum,
+                            nugetVersion: versionNum,
+                            nugetId: `savi.${groupIds[groupId]}.${key}`,
+                            dependencyOnly: !isNotDependency, // Sometimes there is no source to download it's a dependency
+                            downloadJavaSourceJars,
+                            downloadPoms,
+                            downloadJavaDocJars,
+                            downloadMetadataFiles
+                        });
+                    }));
+                    return result;
+
+                }, groupIndex));
+
+
+            function VersionSplit(value, _name) {
+                return value.split(',').reverse();
+            }
         }
-
         return groupIndex;
     }
-}
 }
 
 module.exports = new Handler();
